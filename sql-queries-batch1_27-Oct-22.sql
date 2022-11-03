9bc654cf8ac727a7ea3109dc30b5ff8c28a1875a
ramesh cheppali <ramesh96420@gmail.com>
2022-11-03 09:28:19 +0530

    updated some sql queris

--- sql-queries-batch1_27-Oct-22.sql
+++ sql-queries-batch1_27-Oct-22.sql
@@ -1,3 +1,6 @@
+--git urls
+--https://github.com/SriTejaInstitute02Jun22/sql-database-queries/tree/master
+
 --date 27-Oct-22
 create table company (comp_id number(10), 
 					  comp_name varchar2(20), 
@@ -90,7 +93,7 @@ insert into employee(
   employee_email,
   employee_company_name,
   employee_location)
-values('R2452', 'Ram', 25000.09, 'M', SYSDATE, '+9024422423', 'ram@gmail.com', null, 'Hyderabad');
+values('R7890', 'Ram', 25000.09, 'M', SYSDATE, '+9024422423', 'ram1@gmail.com', null, 'Hyderabad');
 
 
 select * from employee;
@@ -101,7 +104,7 @@ insert into employee(
   employee_gender,
   employee_mobile, 
   employee_email)
-values('V1234', 'Vikram', 'M','+9024422423', 'ram@gmail.com');
+values('V7780', 'Vikram', 'M','+989098734', 'vikram11@gmail.com');
 
 select * from employee;
 
@@ -136,11 +139,141 @@ select * from employee where employee_salary is null;
 
 commit;
 
+--key points
+0.environment setup (oracle softwar installation, access requests(git,jenkins, databases(in diff env (dev, uat, preprod, prod))))
+1.creating the new users.
+2.give the permission to that new users.
+3.diff types of languages in oracle.
+4.how to create new tables and how to delete the table.
+5.how to insert the data into the new table.
+6.how to add new column in existing table and how to delete existing column from the tables.
+7.how to rename the table name and columns name.
+8.select 
+9.where
+10.null and  not null uses while creating the table.
+11.update 
+12.commit 
+13.DISTINCT
+14.count
+15.primary and foreign key
 
 
 
 
+--
+CREATE TABLE current_test (col1 TIMESTAMP WITH TIME ZONE);
+--31-Oct-22
+select * from  employee;
 
+select count(distinct employee_name) from employee;
+
+select distinct employee_id from employee;
+
+select count(*) from employee;
+
+CREATE TABLE mytimestamp (
+     id INTEGER,
+     made_on TIMESTAMP(4)
+   );
+   
+   INSERT INTO mytimestamp (id, made_on) VALUES (
+      1, TIMESTAMP '2005-05-13 07:15:31.123456789'
+    );
+    
+select * from mytimestamp;
+
+--delete the table.
+drop table <table_name>;  --syntax
+drop table MYTIMESTAMP;   --examples
+
+select * from employee;
+
+--how to avoid the dublicate recored while retriving the data form tables.
+--we need to use DISTINCT command to avoid the dublicate recored while retriving the data form tables.
+select distinct employee_id from employee;
+
+select * from employee where employee_id in('R2452','S3443');
+
+select * from employee where employee_id ='R2452';
+
+select * from employee where employee_id ='S3443';
+
+
+select count(*) from employee;
+
+select * from employee;
+
+--31-Oct-22
+--update commands
+--if we want to update the any records in existing table then we need to follow below syntax.
+update <table_name> set <column_names>=<column_values> where <column_names>=<column_values>;    --syntax
+update employee set employee_salary=52000.20 where employee_id='V7780';           --examples
+
+select * from employee where employee_id='V7780';
+
+select * from employee where employee_id='V1234';
+
+update employee set employee_salary='48000.90', employee_joining_date='12-Jan-21',  where employee_id='V1234';
+
+select * from employee where employee_id='V7780';
+
+update employee set employee_joining_date='01-Mar-2021', employee_company_name='Tech Mahendra', employee_location='Chennai' where employee_id='V7780';
+
+commit;
+
+--03-Nov-22
+
+--how to find the total table name form the user.
+select * from tab;
+
+commit; --this is using for to save the data.
+
+--how to add new column in the existing table
+alter table employee add employee_dept varchar2(25);
+
+commit;
+
+select * from employee; --select command is used to get/retrive the data from the table.
+
+--whenever we are going to get the data from the table based on some coditions then we need to use where command.
+--to run the sql commands using "control+enter" shortcut key.
+select * from employee where employee_id='R1022';
+
+--if we want to get the data based on multiple conditions then we need to use and command, like below 
+select * from employee where employee_email='chandra@gmail.com' and employee_mobile='+917896539260';
+
+select * from employee;
+--is command is used to get the data based on some conditions level like is null or is not null.
+select * from employee where employee_company_name is not null;
+
+select * from employee where employee_company_name is null;
+
+select * from employee where employee_salary is null;
+
+select * from employee where employee_salary is not null;
+
+select * from employee where employee_salary is null and employee_dept is not null;
+
+--------------------------------------------------------------------------
+select * from employee;
+
+desc employee;
+
+update employee set employee_company_name='Tech Mahendra', employee_location = 'Pune' where employee_id = 'V1234';
+
+select * from employee where employee_id = 'V1234';
+
+--I want to get employee_id, emailid, mobile number and salary from the employee;
+select employee_id, employee_email, employee_mobile, employee_salary from  employee;
+
+--I want to get employee_id, emailid, mobile number and salary from the employee but whoever employee_salary is having more then 40000.
+select employee_id, employee_email, employee_mobile, employee_salary from employee where employee_salary > 40000;
+
+--I want to get employee_id, emailid, mobile number and salary from the employee but whoever employee_salary is having less than 40000.
+select employee_id, employee_email, employee_mobile, employee_salary from employee where employee_salary < 40000;
+
+--I want to get employee_id, emailid, mobile number and salary from the employee but whoever employee_salary is having more than 40000 and less than 70000;
+select employee_id, employee_email, employee_mobile, employee_salary from employee where employee_salary > 40000 and employee_salary <70000;
 
 
 
